f3a71e6c6d5aec20b2dac36ab1af60151cc20fcc
raulruval <32712264+raulruval@users.noreply.github.com>
2020-03-21 10:24:48 +0100

    it5-v1.0

--- entrenados/lib/pages/search.dart
+++ entrenados/lib/pages/search.dart
@@ -1,6 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:entrenados/models/searchModel.dart';
 import 'package:entrenados/pages/activity.dart';
+import 'package:entrenados/pages/search_post.dart';
 import 'package:flutter/material.dart';
 import 'package:entrenados/models/user.dart';
 import 'package:entrenados/pages/home.dart';
@@ -11,9 +13,11 @@ class Search extends StatefulWidget {
   _SearchState createState() => _SearchState();
 }
 
-class _SearchState extends State<Search> with AutomaticKeepAliveClientMixin<Search> {
+class _SearchState extends State<Search>
+    with AutomaticKeepAliveClientMixin<Search> {
   TextEditingController searchController = TextEditingController();
   Future<QuerySnapshot> searchFutureResults;
+  bool searchuser = false;
 
   handleBuscar(String consulta) {
     Future<QuerySnapshot> users = usersRef
@@ -28,16 +32,20 @@ class _SearchState extends State<Search> with AutomaticKeepAliveClientMixin<Sear
     searchController.clear();
   }
 
-  buildBuscador() {
-    return AppBar(
-      backgroundColor: Colors.white,
-      title: TextFormField(
+  buildSearch() {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.teal[900],
+        ),
+      ),
+
+      child: TextFormField(
         controller: searchController,
         decoration: InputDecoration(
           hintText: "Buscar un instructor...",
-          filled: true,
           prefixIcon: Icon(
-            Icons.account_box,
+            Icons.people,
             size: 28.0,
           ),
           suffixIcon: IconButton(
@@ -47,6 +55,7 @@ class _SearchState extends State<Search> with AutomaticKeepAliveClientMixin<Sear
         ),
         onFieldSubmitted: handleBuscar,
       ),
+
       // bottom: TabBar(
       //   tabs: [
       //     Tab(text: 'Entrenamientos', icon: Icon(Icons.directions_run)),
@@ -57,12 +66,13 @@ class _SearchState extends State<Search> with AutomaticKeepAliveClientMixin<Sear
   }
 
   buildNoContent() {
-    return Container(
-      child: Center(
-        child: ListView(
-          shrinkWrap: true,
-          children: <Widget>[
-            Text(
+    return Expanded(
+      child: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(top: 75.0),
+            child: Text(
               "Encontrar usuarios",
               textAlign: TextAlign.center,
               style: TextStyle(
@@ -72,8 +82,8 @@ class _SearchState extends State<Search> with AutomaticKeepAliveClientMixin<Sear
                 fontSize: 60.0,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -91,41 +101,125 @@ class _SearchState extends State<Search> with AutomaticKeepAliveClientMixin<Sear
             UserResult searchResult = UserResult(user);
             searchResults.add(searchResult);
           });
-          return ListView(
-            children: searchResults,
+          return Expanded(
+            child: ListView(
+              children: searchResults,
+            ),
           );
         }
       },
     );
   }
 
+
   bool get wantKeepAlive => true;
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return Scaffold(
-        appBar: buildBuscador(),
-        body: Center(
-          child: Container(
-            height: double.infinity,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Colors.grey[200],
-                  Colors.grey[350],
-                ],
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.grey[200],
+            title: Text(
+              "Buscar",
+              style: TextStyle(
+                color: Colors.teal,
               ),
             ),
-            child: searchFutureResults == null
-                ? buildNoContent()
-                : buildSearchResults(),
+            bottom: TabBar(
+              unselectedLabelColor: Colors.teal,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicator: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.teal[900],
+              ),
+              tabs: [
+                Tab(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      border: Border.all(
+                        color: Colors.teal[900],
+                        width: 1,
+                      ),
+                    ),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text("Entrenamientos"),
+                    ),
+                  ),
+                ),
+                Tab(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50),
+                      border: Border.all(
+                        color: Colors.teal[900],
+                        width: 1,
+                      ),
+                    ),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text("Entrenadores"),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ));
+          body: TabBarView(children: [
+            Center(
+              child: Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.grey[200],
+                      Colors.grey[350],
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                ),
+                child: SearchPost(SearchModel()),
+              ),
+            ),
+            Center(
+              child: Container(
+                  height: double.infinity,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.grey[200],
+                        Colors.grey[350],
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      children: <Widget>[
+                        buildSearch(),
+                        searchFutureResults == null
+                            ? buildNoContent()
+                            : buildSearchResults(),
+                      ],
+                    ),
+                  )),
+            ),
+          ])),
+    );
   }
 }
+
+
 
 class UserResult extends StatelessWidget {
   final User user;
@@ -135,7 +229,6 @@ class UserResult extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-  
     return Container(
       color: Theme.of(context).primaryColor.withOpacity(0.7),
       child: Column(
---
+++ entrenados/lib/pages/search_post.dart
@@ -0,0 +1,153 @@
+import 'package:entrenados/models/searchModel.dart';
+import 'package:flutter/material.dart';
+
+class SearchPost extends StatefulWidget {
+  final SearchModel searchModel;
+
+  SearchPost(this.searchModel);
+  @override
+  _SearchPostState createState() => _SearchPostState();
+}
+
+class _SearchPostState extends State<SearchPost> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        Container(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 24.0),
+                  child: Text("Dificultad",
+                      style: TextStyle(fontWeight: FontWeight.w800)),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: Wrap(
+                    spacing: MediaQuery.of(context).size.width * 0.0465,
+                    children: [
+                      for (var difficulty in widget.searchModel.difficulty)
+                        ChoiceChip(
+                            selected: widget.searchModel.selectedDifficulty
+                                .contains(difficulty),
+                            selectedColor: Colors.teal[900],
+                            backgroundColor: Colors.teal[100],
+                            labelPadding:
+                                const EdgeInsets.symmetric(horizontal: 16),
+                            labelStyle: widget.searchModel.selectedDifficulty
+                                    .contains(difficulty)
+                                ? TextStyle(color: Colors.white)
+                                : TextStyle(color: Colors.black),
+                            label: Text(difficulty),
+                            onSelected: (isSelected) {
+                              setState(() {
+                                _onDifficultySelected(isSelected, difficulty);
+                              });
+                            }),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 24.0),
+                  child: Text("Duración aproximada",
+                      style: TextStyle(fontWeight: FontWeight.w800)),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: Wrap(
+                    spacing: MediaQuery.of(context).size.width * 0.098,
+                    children: [
+                      for (var duration in widget.searchModel.duration)
+                        ChoiceChip(
+                            selected: widget.searchModel.selectedDuration
+                                .contains(duration),
+                            selectedColor: Colors.teal[900],
+                            backgroundColor: Colors.teal[100],
+                            labelPadding:
+                                const EdgeInsets.symmetric(horizontal: 16),
+                            labelStyle: widget.searchModel.selectedDuration
+                                    .contains(duration)
+                                ? TextStyle(color: Colors.white)
+                                : TextStyle(color: Colors.black),
+                            label: Text(duration),
+                            onSelected: (isSelected) {
+                              setState(() {
+                                _onDurationSelected(isSelected, duration);
+                              });
+                            }),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 24.0),
+                  child: Text("Grupo",
+                      style: TextStyle(fontWeight: FontWeight.w800)),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: Wrap(
+                    spacing: MediaQuery.of(context).size.width * 0.08,
+                    children: [
+                      for (var group in widget.searchModel.group)
+                        ChoiceChip(
+                            selected: widget.searchModel.selectedGroup
+                                .contains(group),
+                            selectedColor: Colors.teal[900],
+                            backgroundColor: Colors.teal[100],
+                            labelPadding:
+                                const EdgeInsets.symmetric(horizontal: 16),
+                            labelStyle: widget.searchModel.selectedDuration
+                                    .contains(group)
+                                ? TextStyle(color: Colors.white)
+                                : TextStyle(color: Colors.black),
+                            label: Text(group),
+                            onSelected: (isSelected) {
+                              setState(() {
+                                _onGroupSelected(isSelected, group);
+                              });
+                            }),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        FloatingActionButton(
+          onPressed: () => print("hola"),
+          elevation: 0.0,
+          backgroundColor: Colors.teal,
+          child: Text("Buscar"),
+        )
+      ],
+    );
+  }
+
+  void _onDifficultySelected(bool isSelected, String difficulty) {
+    isSelected
+        ? widget.searchModel.selectedDifficulty.add(difficulty)
+        : widget.searchModel.selectedDifficulty.remove(difficulty);
+    print(widget.searchModel.selectedDifficulty);
+  }
+
+  void _onDurationSelected(bool isSelected, String duration) {
+    isSelected
+        ? widget.searchModel.selectedDuration.add(duration)
+        : widget.searchModel.selectedDuration.remove(duration);
+    print(widget.searchModel.selectedDuration);
+  }
+
+  void _onGroupSelected(bool isSelected, String group) {
+    isSelected
+        ? widget.searchModel.selectedGroup.add(group)
+        : widget.searchModel.selectedGroup.remove(group);
+    print(widget.searchModel.selectedGroup);
+  }
+}
